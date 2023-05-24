22f92c2bda5dec1994cfaa9e8d5979a5aa754917
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-05-24 14:27:40 +0700

    Main Branch

---
+++ flask_divisiblesumpairs.py
@@ -0,0 +1,25 @@
+import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def divisible_sum_pairs():  # put application's code here
+    n = request.json['n']
+    k = request.json['k']
+    ar = request.json['ar']
+
+    count = 0
+    for i in range(0,len(ar), 1):
+        for j in range(i + 1, len(ar), 1):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+
+    response = {"response": count}
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
